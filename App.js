e369763e12c5e0a9102a184f9a6ca024b30b7bdf
ZAIDANWAR22 <za05404@gmail.com>
2022-09-04 15:07:28 +0500

    changes in Appjs

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 import "react-native-gesture-handler";
 import { LogBox,View } from "react-native";
 import RootNavigator from "./src/routes";
-<<<<<<< HEAD
-// import { Provider } from "react-redux";
-// import { store } from "./src/redux/store";
-=======
+
+
 import { Provider } from "redux";
 import { store } from "./src/redux/store";
 import { useFonts } from "expo-font";
 
 
->>>>>>> aa2bef82ab739a81545f7e49fa68701b2ee8ae09
+
 
 LogBox.ignoreLogs(["VirtualizedLists", "Warning:..."]);
 LogBox.ignoreAllLogs();
