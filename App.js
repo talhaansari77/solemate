08c78cad762000f5d8971446d597760ef749a0bb
talhaansari77 <talhaprince09@gmail.com>
2022-09-04 12:51:38 +0500

    splashScreen

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import "react-native-gesture-handler";
 import { LogBox } from "react-native";
 import RootNavigator from "./src/routes";
-import { Provider } from "redux";
-import { store } from "./src/redux/store";
+// import { Provider } from "react-redux";
+// import { store } from "./src/redux/store";
 
 LogBox.ignoreLogs(["VirtualizedLists", "Warning:..."]);
 LogBox.ignoreAllLogs();
